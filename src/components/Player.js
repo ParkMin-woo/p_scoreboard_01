5044aba6298e1ca2a18393b64af687f763529076
Park Minwoo <lwmwpark3523@gmail.com>
2019-10-05 12:56:02 +0900

    PureComponent로 성능 개선

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Counter from "./Counter";
 
-const Player = (props) => {
-	console.log("props in Player : " , props);
-	return (
-		<div className="player">
-			<span className="player-name">
-				<button className="remove-player" onClick={() => {props.removePlayer(props.id)}} > X </button>
-				{props.name}
-			</span>
-			<Counter score = {props.score} scoreUpDown = {props.scoreUpDown} id = {props.id} />
-		</div>
-	);
+// const Player = (props) => {
+class Player extends React.PureComponent {
+	render() {
+		console.log(this.props.name , " rendered");
+		return (
+			<div className="player">
+				<span className="player-name">
+					<button className="remove-player" onClick={() => {this.props.removePlayer(this.props.id)}} > X </button>
+					{this.props.name}
+				</span>
+				<Counter score = {this.props.score} scoreUpDown = {this.props.scoreUpDown} id = {this.props.id} />
+			</div>
+		);
+	}
 }
 
 export default Player;
