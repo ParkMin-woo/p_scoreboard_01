1997ae0ee1d7b6a48509b80ca32cafbfd164f126
Park Minwoo <lwmwpark3523@gmail.com>
2019-10-05 10:49:19 +0900

    AddPlayerForm - HTML5 validation 체크!

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -18,6 +18,13 @@ class AddPlayerForm extends React.Component {
 		// e.stopPropagation();
 		// Reloading되는 기본 이벤트를 막아줌.
 		e.preventDefault();
+
+		const form = document.getElementById("form");
+		const player = document.getElementById("player");
+
+		console.log(form.checkValidity());
+		console.log(player.validity.valid);
+
 		this.props.addPlayer(this.state.value);
 		this.setState({value : ''});
 	}
@@ -25,8 +32,8 @@ class AddPlayerForm extends React.Component {
 	render() {
 		console.log("this.props in AddPlayerForm : " , this.props);
 		return (
-			<form className="form" onSubmit={this.handleSubmit}>
-				<input className="input" type = "text" placeholder="Enter a Player's Name" value = {this.state.value} onChange = {this.handleValueChange} />
+			<form id = "form" className="form" onSubmit={this.handleSubmit} noValidate>
+				<input id ="player" className="input" type = "text" placeholder="Enter a Player's Name" value = {this.state.value} onChange = {this.handleValueChange} required />
 				<input className="input" type = "submit" value = "Add Player" />
 			</form>
 		);
